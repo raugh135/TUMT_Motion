acc0d940ca14f349f13569688c68ff1b457a75bb
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-17 11:12:07 +0800

    1017

---
+++ UnityMotion/Assets/Script/ArrayOperation.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+public class ArrayOperation : MonoBehaviour {
+    public int studentA = 10;
+    public int studentB = 20;
+    public int studentC = 30;
+
+    public int[] students = { 10,20,30 };
+}
