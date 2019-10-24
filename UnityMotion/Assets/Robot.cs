35f5c86c237279e39b19c6eda309514177f60aa7
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-24 10:11:47 +0800

    Motion10_24

---
+++ UnityMotion/Assets/Robot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot : MonoBehaviour {
+    private Animator ani;
+
+    // Use this for initialization
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ani.SetTrigger("Dancing");
+        }
+
+        ani.SetBool("Run", Input.GetKey(KeyCode.R));
+    }
+}
