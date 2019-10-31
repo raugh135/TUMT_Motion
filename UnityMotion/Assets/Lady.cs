f7d36ddc13ce691295dadbe209fd798b4d72b61b
raugh135 <55518299+raugh135@users.noreply.github.com>
2019-10-31 10:28:34 +0800

    10-31

---
+++ UnityMotion/Assets/Lady.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lady : MonoBehaviour {
+
+	private void Attack()
+    {
+
+    }
+
+    private void Hurt()
+    {
+
+    }
+
+    private void Jump()
+    {
+
+    }
+
+    private void Dead()
+    {
+
+    }
+}
